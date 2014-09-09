c2dfcf30f27ce9f4e584442e059ba76d906fffdb
R. Tyler Croy <tyler@monkeypox.org>
2014-09-09 10:47:22 -0700

    Add support for running integration tests through RSpec against a pre-configured broker/topic
    This will make it a bit easier to run local tests in a more automated fashion
    against some non-public broker hostnames

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'rubygems'
+require 'yaml'
+require 'rspec'
 
 # Add ext/ to the load path so we can load `hermann_lib`
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../ext/'))
@@ -9,4 +11,14 @@ require 'hermann'
 RSpec.configure do |c|
   c.color = true
   c.formatter = :documentation
+
+  shared_context 'integration test context', :type => :integration do
+    let(:topic) { $integrationconf['kafka']['topic'] }
+    let(:brokers) { $integrationconf['kafka']['brokers'] }
+  end
+end
+
+integration_config = File.expand_path(File.dirname(__FILE__) + '/fixtures/integration.yml')
+if File.exists?(integration_config)
+  $integrationconf = YAML.load_file(integration_config)
 end
